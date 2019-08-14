7680976fc38b94b68ec81f94b6866b8badc5a309
Worome <41673409+Worome@users.noreply.github.com>
2019-08-14 11:43:29 +0200

    Terminado

--- app/src/main/java/com/trianacodes/script/practica_radiobutton/MainActivity.java
+++ app/src/main/java/com/trianacodes/script/practica_radiobutton/MainActivity.java
@@ -6,13 +6,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // Creo objetos para almacenar los valores digitados en la interfaz
     private EditText et_num1, et_num2;
     private TextView tv_resultado;
-    private RadioButton rbSum, rbRes;
+    private RadioButton rbSum, rbRes, rbMult, rbDiv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
         tv_resultado = findViewById(R.id.tv_resultado);
         rbSum = findViewById(R.id.rb_suma);
         rbRes = findViewById(R.id.rb_resta);
+        rbMult = findViewById(R.id.rbMultiplicar);
+        rbDiv = findViewById(R.id.rbDividir);
+
     }
 
     public void Calculo(View view){
@@ -43,17 +47,38 @@ public class MainActivity extends AppCompatActivity {
             ejecutar la función Suma. */
             tv_resultado.setText(tv_resultado.getText().toString() + " " + Suma(valor1, valor2));
 
-        } else {
+        } else if (rbRes.isChecked()){
 
             /* Establezco el valor del TextView a su valor unido al valor del cálculo obtenido al
             ejecutar la función Resta. */
             tv_resultado.setText(tv_resultado.getText().toString() + " " + Resta(valor1, valor2));
 
+        } else if(rbMult.isChecked()){
+            /* Establezco el valor del TextView a su valor unido al valor del cálculo obtenido al
+            ejecutar la función Multiplica. */
+            tv_resultado.setText(tv_resultado.getText().toString() + " " + Multiplica(valor1, valor2));
+
+        } else {
+
+            //Compruebo que el valor del divisor no sea 0
+
+            if (valor2 == 0){
+
+                Toast.makeText(this, "El segundo valor no puede ser 0", Toast.LENGTH_LONG).show();
+
+            } else {
+
+                /* Establezco el valor del TextView a su valor unido al valor del cálculo obtenido al
+                ejecutar la función Divide. */
+                tv_resultado.setText(tv_resultado.getText().toString() + " " + Divide(valor1, valor2));
+
+            }
+
         }
 
     }
 
-    // Las funciones Suma y Resta deben recibir dos valores (v1 y v2) para poder hacer el cálculo.
+    // Las funciones Suma, Resta, Multiplica y Divide deben recibir dos valores (v1 y v2) para poder hacer el cálculo.
     public int Suma (int v1, int v2){
 
         // Devuelve el resultado de la operación
@@ -65,6 +90,20 @@ public class MainActivity extends AppCompatActivity {
 
         // Devuelve el resultado de la operación
         return v1 - v2;
+
+    }
+
+    public int Multiplica (int v1, int v2){
+
+        // Devuelve el resultado de la operación
+        return v1 * v2;
+
+    }
+
+    public int Divide (int v1, int v2){
+
+        // Devuelve el resultado de la operación
+        return v1 / v2;
 
     }
 
